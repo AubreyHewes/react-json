072fe406c19193dbaece4e76dfbf79bf8d9a4cd1
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2019-12-18 19:06:19 +0100

    add theme skeleton

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,12 @@
 import { configure } from "@storybook/react";
 
+// import { themes } from '@storybook/theming';
+//
+// // Option defaults.
+// addParameters({
+//   options: {
+//     theme: themes.dark,
+//   },
+// });
+
 configure(require.context("../src", true, /\.stories\.(js|tsx?)$/), module);
